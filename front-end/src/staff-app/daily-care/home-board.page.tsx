656dc22d835419f180ae1c8f151f53b1fa77017e
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-09-05 11:08:49 +0530

    refactored home-board.page with toolbar component

--- front-end/src/staff-app/daily-care/home-board.page.tsx
+++ front-end/src/staff-app/daily-care/home-board.page.tsx
@@ -1,15 +1,14 @@
 import React, { useEffect } from "react"
 import styled from "styled-components"
-import Button from "@material-ui/core/ButtonBase"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { Spacing, BorderRadius, FontWeight } from "shared/styles/styles"
-import { Colors } from "shared/styles/colors"
+import { Spacing } from "shared/styles/styles"
 import { CenteredContainer } from "shared/components/centered-container/centered-container.component"
 import { Person } from "shared/models/person"
 import { useApi } from "shared/hooks/use-api"
 import { StudentListTile } from "staff-app/components/student-list-tile/student-list-tile.component"
 import { ActiveRollOverlay, ActiveRollAction } from "staff-app/components/active-roll-overlay/active-roll-overlay.component"
 import { useStaffAppState } from "staff-app/context/staffAppContext"
+import { Toolbar, ToolbarAction } from "staff-app/components/toolbar/toolbar.component"
 
 export const HomeBoardPage: React.FC = () => {
   const {
@@ -68,43 +67,11 @@ export const HomeBoardPage: React.FC = () => {
   )
 }
 
-type ToolbarAction = "roll" | "sort"
-interface ToolbarProps {
-  onItemClick: (action: ToolbarAction, value?: string) => void
-}
-const Toolbar: React.FC<ToolbarProps> = (props) => {
-  const { onItemClick } = props
-  return (
-    <S.ToolbarContainer>
-      <div onClick={() => onItemClick("sort")}>First Name</div>
-      <div>Search</div>
-      <S.Button onClick={() => onItemClick("roll")}>Start Roll</S.Button>
-    </S.ToolbarContainer>
-  )
-}
-
 const S = {
   PageContainer: styled.div`
     display: flex;
     flex-direction: column;
     width: 50%;
     margin: ${Spacing.u4} auto 140px;
-  `,
-  ToolbarContainer: styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    color: #fff;
-    background-color: ${Colors.blue.base};
-    padding: 6px 14px;
-    font-weight: ${FontWeight.strong};
-    border-radius: ${BorderRadius.default};
-  `,
-  Button: styled(Button)`
-    && {
-      padding: ${Spacing.u2};
-      font-weight: ${FontWeight.strong};
-      border-radius: ${BorderRadius.default};
-    }
   `,
 }
