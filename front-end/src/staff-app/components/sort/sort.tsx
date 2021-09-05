650c97c70cac1019f05afab6ff65fc423c600d24
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-09-05 18:17:45 +0530

    refactored sort component to use Context

--- front-end/src/staff-app/components/sort/sort.tsx
+++ front-end/src/staff-app/components/sort/sort.tsx
@@ -4,29 +4,28 @@ import { FontSize, Spacing } from "shared/styles/styles"
 import FormGroup from "@material-ui/core/FormGroup"
 import FormControlLabel from "@material-ui/core/FormControlLabel"
 import Switch from "@material-ui/core/Switch"
+import { useStaffAppState } from "staff-app/context/staffAppContext"
 
 export const Sort: React.FC = () => {
-  const [showSortTypes, setShowSortTypes] = React.useState(false)
-  const [state, setState] = React.useState({
-    firstName: true,
-    ascending: false,
-  })
+  const {
+    dispatch,
+    state: {
+      sort: { applied, firstName, ascending },
+    },
+  } = useStaffAppState()
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setState({ ...state, [event.target.name]: event.target.checked })
-  }
   return (
     <S.SortContainer>
-      <FormControlLabel control={<Switch checked={showSortTypes} onChange={() => setShowSortTypes(!showSortTypes)} name="firstName" />} label="Sort Users" labelPlacement="start" />
-      {showSortTypes && (
+      <FormControlLabel control={<Switch checked={applied} onChange={() => dispatch({ type: "TOGGLE_SORT" })} name="firstName" />} label="Sort Users" labelPlacement="start" />
+      {applied && (
         <FormGroup row>
           <FormControlLabel
-            control={<Switch size="small" checked={state.firstName} onChange={handleChange} name="firstName" />}
-            label={state.firstName ? "First Name" : "Last Name"}
+            control={<Switch size="small" checked={firstName} onChange={() => dispatch({ type: "SORT_BY_FIRSTNAME_OR_LASTNAME" })} name="firstName" />}
+            label={firstName ? "First Name" : "Last Name"}
           />
           <FormControlLabel
-            control={<Switch size="small" checked={state.ascending} onChange={handleChange} name="ascending" />}
-            label={state.ascending ? "Ascending" : "Descending"}
+            control={<Switch size="small" checked={ascending} onChange={() => dispatch({ type: "SORT_BY_ASCENDING_OR_DESCENDING" })} name="ascending" />}
+            label={ascending ? "Ascending" : "Descending"}
           />
         </FormGroup>
       )}
