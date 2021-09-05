38f1860a1a8d6ae59d10d6b7ab8abe84defee9aa
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-09-05 18:16:43 +0530

    added sort action types

--- front-end/src/staff-app/reducer/staffAppActions.ts
+++ front-end/src/staff-app/reducer/staffAppActions.ts
@@ -1,2 +1,5 @@
-export type IActionType = { type: "CHANGE_ROLL_MODE"; payload: boolean }
-
+export type IActionType =
+  | { type: "CHANGE_ROLL_MODE"; payload: boolean }
+  | { type: "TOGGLE_SORT" }
+  | { type: "SORT_BY_FIRSTNAME_OR_LASTNAME" }
+  | { type: "SORT_BY_ASCENDING_OR_DESCENDING" }
