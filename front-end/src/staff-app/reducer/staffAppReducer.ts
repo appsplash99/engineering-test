ec531a75b693f93f128aa06e294163e8f5bfd8fb
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-09-05 18:17:07 +0530

    added sort actions

--- front-end/src/staff-app/reducer/staffAppReducer.ts
+++ front-end/src/staff-app/reducer/staffAppReducer.ts
@@ -6,6 +6,24 @@ export const staffAppReducer = (prevState: InitialState, action: IActionType) =>
     case "CHANGE_ROLL_MODE":
       return { ...prevState, isRollMode: action.payload }
 
+    case "TOGGLE_SORT":
+      return {
+        ...prevState,
+        sort: { ...prevState.sort, applied: !prevState.sort.applied },
+      }
+
+    case "SORT_BY_FIRSTNAME_OR_LASTNAME":
+      return {
+        ...prevState,
+        sort: { ...prevState.sort, firstName: !prevState.sort.firstName },
+      }
+
+    case "SORT_BY_ASCENDING_OR_DESCENDING":
+      return {
+        ...prevState,
+        sort: { ...prevState.sort, ascending: !prevState.sort.ascending },
+      }
+
     default:
       console.log("THIS ACTION IS NOT PRESENT IN STAFF-APP-REDUCER")
       return prevState
