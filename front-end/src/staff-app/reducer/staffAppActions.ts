593d558401bf7623b532469ebf67d9b0d138ba2a
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-09-08 00:48:48 +0530

    updated actions for task 2

--- front-end/src/staff-app/reducer/staffAppActions.ts
+++ front-end/src/staff-app/reducer/staffAppActions.ts
@@ -1,3 +1,5 @@
+import { UpdatedStudent } from "staff-app/context/staffAppContext.type"
+
 export type IActionType =
   | { type: "CHANGE_ROLL_MODE"; payload: boolean }
   | { type: "TOGGLE_SORT" }
@@ -5,4 +7,7 @@ export type IActionType =
   | { type: "SORT_BY_ASCENDING_OR_DESCENDING" }
   | { type: "CHANGE_SEARCH_STRING"; payload: string }
   | { type: "RESET_SEARCH_STRING" }
+  /** TODO: MIGHT NEED TO REMOVE BELOW LINE */
   | { type: "SET_ALL_STUDENTS_COUNT"; payload: number }
+  /** TODO: MIGHT NEED TO REMOVE ABOVE LINE */
+  | { type: "ADD_OR_UPDATE_STUDENT_INTO_UPDATED_STUDENT_ROLLS"; payload: UpdatedStudent }
