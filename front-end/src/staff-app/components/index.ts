cfe095a6b8c8092cfd607d6a166e461182ffbaf5
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-09-07 13:11:59 +0530

    updated component exports

---
+++ front-end/src/staff-app/components/index.ts
@@ -0,0 +1,11 @@
+import { ActiveRollOverlay } from "./active-roll-overlay/active-roll-overlay.component"
+import { Header } from "./header/header.component"
+import { RollStateIcon } from "./roll-state/roll-state-icon.component"
+import { RollStateList } from "./roll-state/roll-state-list.component"
+import { RollStateSwitcher } from "./roll-state/roll-state-switcher.component"
+import { Search } from "./search/search"
+import { Sort } from "./sort/sort"
+import { StudentListTile } from "./student-list-tile/student-list-tile.component"
+import { Toolbar } from "./toolbar/toolbar.component"
+
+export { ActiveRollOverlay, Header, RollStateIcon, RollStateList, RollStateSwitcher, Search, Sort, StudentListTile, Toolbar }
