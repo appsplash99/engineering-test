f44d832f8088e314e922faac205f5cd961a51d85
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-09-08 19:16:48 +0530

    added transform Array util

--- front-end/src/staff-app/utils/array-functions.ts
+++ front-end/src/staff-app/utils/array-functions.ts
@@ -1,6 +1,11 @@
 import { Person } from "shared/models/person"
+import { RolllStateType } from "shared/models/roll"
 import { UpdatedStudent } from "staff-app/context/staffAppContext.type"
 
 export const isStudentInUpdatedStudentRolls = (updatedStudentRolls: UpdatedStudent[], student: Person) => {
   return updatedStudentRolls.some((item) => item?.id === student?.id)
+}
+
+export const transformArrayToRollInputObj = (updatedStudentRolls: UpdatedStudent[]): { student_id: number; roll_state: RolllStateType }[] => {
+  return updatedStudentRolls.map((stuObj) => ({ student_id: stuObj.id, roll_state: stuObj.type }))
 }
--- front-end/src/staff-app/utils/index.ts
+++ front-end/src/staff-app/utils/index.ts
@@ -1,6 +1,6 @@
-import { isStudentInUpdatedStudentRolls } from "./array-functions"
+import { isStudentInUpdatedStudentRolls, transformArrayToRollInputObj } from "./array-functions"
 import { getFilteredStudents } from "./filter-function"
 import { getSearchedStudents } from "./search-function"
 import { getSortedStudents } from "./sort-function"
 
-export { getSearchedStudents, getSortedStudents, isStudentInUpdatedStudentRolls, getFilteredStudents }
+export { getSearchedStudents, getSortedStudents, isStudentInUpdatedStudentRolls, getFilteredStudents, transformArrayToRollInputObj }
